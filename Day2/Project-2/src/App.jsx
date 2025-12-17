5d18aa4fba7e9acc3766cc85e7c39cf17d666030
Potturu Vidyavathi <vidyakavya311@gmail.com>
2025-12-17 22:23:40 +0530

    Sample Code

---
+++ Day2/Project-2/src/App.jsx
@@ -0,0 +1,30 @@
+
+
+// function App() {
+  
+//   return (
+//     <>
+//       <h1>Hello, Welocome to DAY 2 of REACT JS</h1>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+/*  jsx is returns only a single element to return multiple elements we use div tag */
+
+
+// function App() {
+
+//   return (
+//     <>
+//     <div>
+//       <h1>Hello, Good Morning! </h1>
+//       <p>Welocome to DAY 2 of REACT JS</p>
+//     </div>
+//     </>
+//   )
+// }
+
+// export default App
