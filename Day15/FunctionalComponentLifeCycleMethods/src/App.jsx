1fd52653290fb32e2308f6d541249ecabd945a83
Potturu Vidyavathi <vidyakavya311@gmail.com>
2026-01-27 11:28:22 +0530

    Axios in ClassComponent & FunctionalComponent

---
+++ Day15/FunctionalComponentLifeCycleMethods/src/App.jsx
@@ -0,0 +1,15 @@
+// import { Component1 } from "./AxiosClassComponent/Component1"
+import { Component2 } from "./AxiosFunctionComponent/Component2"
+
+
+function App() {
+
+  return (
+    <>
+      {/* <Component1 /> */}
+      <Component2 />
+    </>
+  )
+}
+
+export default App
